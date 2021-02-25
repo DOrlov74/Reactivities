603cea0c03b2355d8a051ca66ada9d78e16d37b5
bukadan <bukadan@gmail.com>
2021-02-25 19:28:09 +0000

    second commit

---
+++ Persistence/Migrations/20210225183648_second.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Discription",
+                table: "Activities",
+                newName: "Description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Activities",
+                newName: "Discription");
+        }
+    }
+}
